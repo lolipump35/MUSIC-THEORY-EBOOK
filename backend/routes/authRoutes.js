20f71001e882c606c88b28e36e5ac44652d8a9ab
lolipump-35 <music.theory35@gmail.com>
2025-05-08 09:59:09 +0200

    update const { registerUser } = require('../controllers/authController');

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // ✅ Import de la fonction registerUser
-const { registerUser } = require('../controllers/authcontroller');
+const { registerUser } = require('../controllers/authController');
 
 // ✅ Route POST vers /api/auth/register
 router.post('/register', registerUser);
