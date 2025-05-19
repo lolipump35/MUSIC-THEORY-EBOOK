061fe05eb20afd6b98d8d5ed94fd4f40c81637f8
lolipump-35 <music.theory35@gmail.com>
2025-05-19 15:36:53 +0200

    uppdate prob url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 5000;
 
 const corsOptions = {
-  origin: 'http://127.0.0.1:5500', // Permet uniquement cette origine
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'], // Permet uniquement cette origine
   credentials: true, // Permet les cookies et les credentials
 };
 
