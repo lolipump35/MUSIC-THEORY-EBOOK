5ba846098ddaaf9bcbbb9e1c22796e1674606001
lolipump-35 <music.theory35@gmail.com>
2025-05-19 14:38:18 +0200

    uppdate password ok

---
+++ backend/test-buffer.js
@@ -0,0 +1,5 @@
+const testPassword = "Diabolo&cie091102";
+
+console.log("Mot de passe (visible) :", testPassword);
+console.log("Longueur :", testPassword.length);
+console.log("Buffer :", Buffer.from(testPassword));
