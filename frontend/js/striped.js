2a806e47a3b18c83946753dd290da5fa2b951dac
lolipump-35 <music.theory35@gmail.com>
2025-09-21 20:57:10 +0200

    url strip render update

--- frontend/js/striped.js
+++ frontend/js/striped.js
@@ -1,26 +1,23 @@
 // #region stripe
 
     // Initialise Stripe avec ta clé publique
-const stripe = Stripe ('pk_test_51RYS7qQr4BGbM3NUgolVTu74BSzvcNJdQ9uklMUiKzCCF2fx1GszkRPWdcFwhDvcCSo9pg4EJJ7nG02m6blyLk9I00mePJTzRH'); // remplace par ta clé publique
+const stripe = Stripe('pk_test_51RYS7qQr4BGbM3NUgolVTu74BSzvcNJdQ9uklMUiKzCCF2fx1GszkRPWdcFwhDvcCSo9pg4EJJ7nG02m6blyLk9I00mePJTzRH');
 
 document.getElementById("buyButtonFormation").addEventListener("click", () => {
-  fetch("http://localhost:5000/create-checkout-session", {
+  const backendUrl = window.location.hostname === "localhost"
+    ? "http://localhost:5000"
+    : "https://mon-backend.onrender.com";
+
+  fetch(`${backendUrl}/create-checkout-session`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
   })
-  .then(res => res.json())
-  .then(data => {
-  console.log("Session data:", data);
-  window.location.href = data.url; // redirection vers Stripe
-})
-  .then(result => {
-    if (result.error) {
-      console.error(result.error.message);
-    }
-  })
-  .catch(err => console.error("Erreur Stripe :", err));
+    .then(res => res.json())
+    .then(data => {
+      console.log("Session data:", data);
+      window.location.href = data.url; // redirection vers Stripe Checkout
+    })
+    .catch(err => console.error("Erreur Stripe :", err));
 });
 
 // #endregion stripe
